79829243ceee63798973e9031006ce83a27737fb
Umar <arrhammzain@gmail.com>
2025-01-28 14:29:35 +0500

    Revert "avoid email verification"
    This reverts commit 2c04e794f3016fa83a1fd487da5339d377dd010b.

--- lib/auth/organisation.ts
+++ lib/auth/organisation.ts
@@ -28,9 +28,7 @@ export async function createUserWithOrganisation(input: {
           password: input.hashedPassword,
           role: Role.ADMIN,
           locale,
-          completedOnboarding: false,
-          // add emailVerified for previous day
-          emailVerified: new Date(Date.now() - 24 * 60 * 60 * 1000)
+          completedOnboarding: false
         }
       }
     },
